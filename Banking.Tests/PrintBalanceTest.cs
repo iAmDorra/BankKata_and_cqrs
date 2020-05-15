4fb72014040e5c5255318b1905eebcfa696a4e5b
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2020-05-15 12:49:44 +0200

    Check if the case with two withdraws is ok

--- Banking.Tests/PrintBalanceTest.cs
+++ Banking.Tests/PrintBalanceTest.cs
@@ -100,5 +100,26 @@ namespace Banking.Tests
                 new AccountStatement(today, 100, 100),
                 new AccountStatement(today, -100, 0));
         }
+
+        [TestMethod]
+        public void ShouldReturnTwoStatementsWithNegativeAmountsAndCalculatedBalanceWhenHavingTwoWithdraw()
+        {
+            DateTime today = DateTime.Now;
+            ITransactions transactions = Substitute.For<ITransactions>();
+            List<ITransaction> noTransactions = new List<ITransaction>
+            {
+                new Withdraw(today, 100),
+                new Withdraw(today, 100)
+            };
+            transactions.GetAll().Returns(noTransactions);
+            var bankingService = new BankingService(transactions);
+
+            var balance = bankingService.PrintBalance();
+
+            Check.That(balance.GetAccountStatements())
+                .ContainsExactly(
+                new AccountStatement(today, -100, -100),
+                new AccountStatement(today, -100, -200));
+        }
     }
 }
